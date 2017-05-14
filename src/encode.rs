0f055a936d122a532eff862b2724c54bd5714c95
SAWADA Tadashi <cesare@mayverse.jp>
2017-05-14 17:49:22 +0900

    Enable to encode a struct with keys

--- src/encode.rs
+++ src/encode.rs
@@ -1,18 +1,35 @@
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
+extern crate rmp;
 extern crate rmp_serde as rmps;
 
 use std::collections::HashMap;
 use std::fs::File;
+use std::io::Write;
 
 use serde::Serialize;
+use rmp::Marker;
+use rmp::encode::{ValueWriteError, write_map_len, write_str};
 use rmps::Serializer;
+use rmps::encode::VariantWriter;
 
 #[derive(Debug, PartialEq, Serialize)]
 pub struct Foo {
     foo: String,
     bar: HashMap<String, i32>,
+}
+
+struct StructMapWriter;
+
+impl VariantWriter for StructMapWriter {
+    fn write_struct_len<W: Write>(&self, wr: &mut W, len: u32) -> Result<Marker, ValueWriteError> {
+        write_map_len(wr, len)
+    }
+
+    fn write_field_name<W: Write>(&self, wr: &mut W, key: &str) -> Result<(), ValueWriteError> {
+        write_str(wr, key)
+    }
 }
 
 fn main() {
@@ -26,5 +43,6 @@ fn main() {
     };
 
     let mut file = File::create("foo.mp").unwrap();
-    foo.serialize(&mut Serializer::new(&mut file)).unwrap();
+    let mut serializer = Serializer::with(&mut file, StructMapWriter);
+    foo.serialize(&mut serializer).unwrap();
 }
